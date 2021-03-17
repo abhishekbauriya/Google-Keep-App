d5c139bbc93dd2009b1072f2d4180246bab62c51
abhishekbauriya <abhishekbauriya007@gmail.com>
2021-03-17 18:25:53 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer(props) {
+    return (
+        <div className="footer">
+            <p>© Programming with ♥ Abhishek</p>
+        </div>
+    );
+}
+
+export default Footer;
